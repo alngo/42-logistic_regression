20b476cb0943ce054cb2b7b44abbc84a190295f5
alngo <alngo@student.42.fr>
2020-03-02 15:59:06 +0100

    feat(describe): build pseudo code.

--- describe.py
+++ describe.py
@@ -6,11 +6,12 @@
 #    By: alngo <alngo@student.42.fr>                +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/03/02 14:51:23 by alngo             #+#    #+#              #
-#    Updated: 2020/03/02 15:16:39 by alngo            ###   ########.fr        #
+#    Updated: 2020/03/02 15:58:42 by alngo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import argparse
+import pandas as pd
 from utils.csv import read_csv
 
 def arguments():
@@ -22,10 +23,52 @@ def arguments():
     args = parser.parse_args()
     return args
 
+def count():
+    pass
+
+def mean():
+    pass
+
+def std():
+    pass
+
+def percentile(percent):
+    pass
+
+def minimum():
+    pass
+
+def maximum():
+    pass
+
 def describe():
     args = arguments()
     df = read_csv(args.datapath)
+    df.drop(["Index"], axis=1, inplace=True)
+    description = pd.DataFrame(
+            columns=df.columns,
+            index=[
+                "Count",
+                "Mean",
+                "Std",
+                "Min",
+                "25%",
+                "50%",
+                "75%",
+                "Max"
+                ]
+            )
+
+    for desc, feature in description.iterrows():
+        if desc is "Min":
+            print("min")
+        if desc is "Max":
+            print("max")
+
+
     # for exemple purpose only
+    print(description.head())
+    print(df.head())
     print(df.describe())
 
 
